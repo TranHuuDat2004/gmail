2f0f3922980ced6dee6ce9aa7bdfdd4efc8608ca
hiddcten <xuantruong3204.vietnam@gmail.com>
2025-05-30 00:39:54 +0700

    connect to firebase

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:gmail/main.dart';
+import 'package:gmail/my_app.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
